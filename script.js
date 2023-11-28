f04e1d7ae7c3ecdbf0976064b5b5942cde619aa0
monfortbrian <monfortnkurunziza0@gmail.com>
2023-11-28 11:53:54 +0200

    second commit

--- script.js
+++ script.js
@@ -30,3 +30,52 @@ loginBtn.addEventListener("click", (e) => {
     e.preventDefault();
     formContainer.classList.remove("active");
 });
+
+const toggleBtn = document.querySelector(".toggle_btn");
+const toggleBtnIcon = document.querySelector(".toggle_btn i");
+const dropDownMenu = document.querySelector(".dropdown_menu");
+
+toggleBtn.onclick = function() {
+    dropDownMenu.classList.toggle("open");
+    const isOpen = dropDownMenu.classList.contains("open");
+
+    toggleBtnIcon.classList = isOpen ?
+        "fa-solid fa-xmark" :
+        "fa-solid fa-bars";
+
+
+    const OpenBtn = document.querySelector("#open"),
+        home = document.querySelector(".home"),
+        formContainer = document.querySelector(".form_container"),
+        formCloseBtn = document.querySelector(".form_close"),
+        signupBtn = document.querySelector("#signup"),
+        loginBtn = document.querySelector("#login"),
+        pwShowHide = document.querySelectorAll(".pw_hide");
+
+    OpenBtn.addEventListener("click", () => home.classList.add("show"));
+    formCloseBtn.addEventListener("click", () => home.classList.remove("show"));
+
+    pwShowHide.forEach((icon) => {
+        icon.addEventListener("click", () => {
+            let getPwInput = icon.parentElement.querySelector("input");
+            if (getPwInput.type === "password") {
+                getPwInput.type = "text";
+                icon.classList.replace("uil-eye-slash", "uil-eye");
+            } else {
+                getPwInput.type = "password";
+                icon.classList.replace("uil-eye", "uil-eye-slash");
+            }
+        });
+    });
+
+    signupBtn.addEventListener("click", (e) => {
+        e.preventDefault();
+        formContainer.classList.add("active");
+    });
+    loginBtn.addEventListener("click", (e) => {
+        e.preventDefault();
+        formContainer.classList.remove("active");
+    });
+
+
+};
